bb1ad0657d6d58b517de962c37237c484d9137b9
mg143pavankumar <pavansharma.mg143@gmail.com>
2023-07-20 19:40:16 +0530

    added AWS-authentication

--- core/images.js
+++ core/images.js
@@ -1,5 +1,7 @@
 import heroSectionImg from "../public/images/img2.svg";
 import lightLogoImg from "../public/images/lightLogo.png";
+import darkLogoImg from "../public/images/darkLogo.png";
+
 import featureImg1 from "../public/images/img-3.webp";
 import featureImg2 from "../public/images/img-1.webp";
 import featureImg3 from "../public/images/img-2.webp";
@@ -9,6 +11,7 @@ import featureImg5 from "../public/images/img-5.png";
 export const images = {
   heroSectionImg,
   lightLogoImg,
+  darkLogoImg,
   featureImg1,
   featureImg2,
   featureImg3,
---
+++ pages/signInSignUp.js
@@ -0,0 +1,61 @@
+import { Amplify, Auth } from "aws-amplify";
+
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import Image from "next/image";
+import { images } from "../core/images";
+import Link from "next/link";
+
+// import awsExports from "./aws-exports";
+// Amplify.configure(awsExports);
+
+export default function SignInSignUp() {
+  const services = {
+    async handleSignUp(formData) {
+      let { username, password, attributes } = formData;
+      username = username.toLowerCase();
+      attributes.email = attributes.email.toLowerCase();
+
+      return Auth.signUp({
+        username,
+        password,
+        attributes,
+        autoSignIn: {
+          enabled: true,
+        },
+      });
+    },
+  };
+
+  return (
+    <>
+      <div className="flex items-center w-[85%] mx-auto pt-5">
+        <div className="w-[29px] md:w-[40px]">
+          <Image
+            src={images.darkLogoImg}
+            alt="FormAi-logo"
+            width="100%"
+            className="object-fill"
+          />
+        </div>
+        <Link href={"/"} className="flex items-center">
+          <span className="text-2xl md:text-3xl font-semibold text-black">
+            FormyAI
+          </span>
+        </Link>
+      </div>
+      <Authenticator
+        services={services}
+        initialState="signUp"
+        className="h-screen -mt-10"
+      >
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
+    </>
+  );
+}
