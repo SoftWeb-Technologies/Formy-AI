4f4809667393e031f46d7a641ca3c9c7ac0e8633
Darshan Mandade <70362500+Darshancodes@users.noreply.github.com>
2023-07-29 22:14:52 +0530

    Update update.ts

--- pages/api/forms/[id]/update.ts
+++ pages/api/forms/[id]/update.ts
@@ -24,7 +24,7 @@ export default (async function handler(
             },
             body: JSON.stringify({
                 operation: 'update',
-                schema: 'typiform',
+                schema: 'formy',
                 table: 'forms',
                 records: [body],
             }),
